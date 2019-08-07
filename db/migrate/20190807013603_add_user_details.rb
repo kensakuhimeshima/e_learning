a84462934b1f2ee786550c5eb855f2337dfec317
kensakuhimeshima <n.w.a.rndd@gmail.com>
2019-08-07 11:23:20 +0800

    [Function]Create sign-up

---
+++ db/migrate/20190807013603_add_user_details.rb
@@ -0,0 +1,9 @@
+class AddUserDetails < ActiveRecord::Migration[5.2]
+  def change
+    add_column :users, :name, :string
+    add_column :users, :email, :string
+    add_column :users, :avatar, :string
+    add_column :users, :is_admin, :boolean
+    add_column :users, :password_digest, :string
+  end
+end
