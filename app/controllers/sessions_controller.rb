aae2e1d14ec1ed1b5aaa8b413ca1922b70b6d1b4
kensakuhimeshima <53370846+kensakuhimeshima@users.noreply.github.com>
2019-08-08 09:43:25 +0800

    [Function] Create Log-in (#5)
    * [Function] Create Log-in
    
    * fixed spacing

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,4 +1,17 @@
 class SessionsController < ApplicationController
   def new
   end
+
+  def create
+    @user = User.find_by(email: params[:session][:email])
+
+    if @user && @user.authenticate(params[:session][:password])
+      log_in @user
+      flash[:info] = "Successfully logged in"
+      redirect_to @user
+    else
+      flash.now[:danger] = "Invalid Credentials"
+      render 'new'
+    end
+  end
 end
