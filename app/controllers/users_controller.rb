68ca72abb3bad78891f0ea32b77a952b4e06cfd0
kensakuhimeshima <n.w.a.rndd@gmail.com>
2019-08-09 12:29:03 +0800

    [Function]Profile edit page

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -21,6 +21,16 @@ class UsersController < ApplicationController
     @user = User.find(params[:id])
   end
 
+  def update
+    @user = User.find(params[:id])
+
+    if @user.update(users_params)
+      redirect_to @user
+    else
+      render 'edit'
+    end
+  end
+
   private
   def users_params
     params.require(:user).permit(:name, :email, :password, :password_comfirmation)
