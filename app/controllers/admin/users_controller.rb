fc51bd8c9e311e235a49c25e4f1b1330a741ccd5
kensakuhimeshima <53370846+kensakuhimeshima@users.noreply.github.com>
2019-08-13 12:36:42 +0800

    [Layout]Admin users list page (#13)

--- app/controllers/admin/users_controller.rb
+++ app/controllers/admin/users_controller.rb
@@ -1,4 +1,22 @@
 class Admin::UsersController < ApplicationController
   def home
   end
+
+  def index
+    @users = User.paginate(page: params[:page], per_page: 10)
+  end
+
+  def update
+    @user = User.find(params[:id])
+    @user.update(is_admin: true)
+
+    redirect_to admin_users_url
+  end
+
+  def destroy
+    @user = User.find(params[:id])
+    @user.update(is_admin: false)
+
+    redirect_to admin_users_url
+  end
 end
