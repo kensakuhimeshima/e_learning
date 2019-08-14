211ce63fc586c75306f546fad11a324aa31c1fcf
kensakuhimeshima <53370846+kensakuhimeshima@users.noreply.github.com>
2019-08-14 11:02:11 +0800

    [Function]Create category (#15)
    * [Function]Create category
    
    * fixed indentation

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_08_07_013603) do
+ActiveRecord::Schema.define(version: 2019_08_14_020612) do
+
+  create_table "categories", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
+    t.string "title"
+    t.text "description"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "users", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.datetime "created_at", null: false
