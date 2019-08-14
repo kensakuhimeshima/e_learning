194c6c645b32f71ccf2f68dcf5cd1ff95c74237d
kensakuhimeshima <n.w.a.rndd@gmail.com>
2019-08-14 10:46:41 +0800

    [Function]Create category

--- app/controllers/admin/categories_controller.rb
+++ app/controllers/admin/categories_controller.rb
@@ -1,7 +1,24 @@
 class Admin::CategoriesController < ApplicationController
   def new
+    @category = Category.new
+  end
+
+  def create
+    @category = Category.new(categories_params)
+    
+    if @category.save
+      flash[:success] = "Category successfully created"
+      redirect_to admin_categories_url
+    else
+      render 'new'
+    end 
   end
 
   def index
+  end
+
+  private
+  def categories_params
+    params.require(:category).permit(:title, :description)
   end
 end
