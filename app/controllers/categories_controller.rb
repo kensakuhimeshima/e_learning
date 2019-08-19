c7d65986f704c584f66ec8368020a38339ad0206
kensakuhimeshima <n.w.a.rndd@gmail.com>
2019-08-19 09:31:47 +0800

    Deleted categories_params

--- app/controllers/categories_controller.rb
+++ app/controllers/categories_controller.rb
@@ -2,9 +2,4 @@ class CategoriesController < ApplicationController
   def index
     @categories = Category.paginate(page: params[:page], per_page: 6)
   end
-
-  private
-  def categories_params
-    params.require(:category).permit(:title, :description)
-  end
 end
