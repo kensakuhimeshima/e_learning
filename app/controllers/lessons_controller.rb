65e830289f1c736738577451ed5b1c3770df05c0
kensakuhimeshima <n.w.a.rndd@gmail.com>
2019-08-20 12:23:38 +0800

    [Layout]Show result page

--- app/controllers/lessons_controller.rb
+++ app/controllers/lessons_controller.rb
@@ -1,12 +1,19 @@
 class LessonsController < ApplicationController
   def create
-    @lesson = Lesson.new(lesson_params)
-    if @lesson.save
-      redirect_to new_lesson_answer_url(@lesson)
+    @lesson = Lesson.find_by(category_id:params[:category_id], user_id: current_user.id)
+    unless @lesson
+      @lesson = Lesson.new(lesson_params)
+      if @lesson.save
+        redirect_to new_lesson_answer_url(@lesson)
+      end
+    else
+      @lesson.destroy
     end
   end
 
   def show
+    @lesson = Lesson.find(params[:id])
+    @result = @lesson.correct_answers.count
   end
 
   private
