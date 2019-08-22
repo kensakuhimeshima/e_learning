5f22d96733b26013995ae20b473b2e5f4e9ef9c7
kensakuhimeshima <n.w.a.rndd@gmail.com>
2019-08-22 09:18:23 +0800

    [Function]Follow

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,17 @@
 class User < ApplicationRecord
   has_many :lessons, dependent: :destroy
   
+  has_many :active_relationships, class_name: "Relationship",
+    foreign_key: "follower_id",
+    dependent: :destroy
+  
+  has_many :passive_relationships, class_name: "Relationship",
+    foreign_key: "followed_id",
+    dependent: :destroy
+
+  has_many :following, through: :active_relationships,source: :followed
+  has_many :followers, through: :passive_relationships,source: :follower
+
   has_secure_password
   before_save { email.downcase! }
 
@@ -17,5 +28,17 @@ class User < ApplicationRecord
 
   def lesson_taken(category_id)
     lesson = lessons.find_by(category_id:category_id)
+  end
+
+  def follow(user)
+    following << user
+  end
+
+  def unfollow(user)
+    following.delete(user)
+  end
+
+  def following?(user)
+    following.include?(user)
   end
 end
